b7413ccf8262f92bf12b1481d59a2e26bd875576
Joseph Drake <jzdrake440@gmail.com>
2018-02-25 01:51:05 -0500

    Added AutoMapper Mappings for BLL<-->DAL

--- WideWorldImportersBLL/AutoMapper/BusinessLogicProfile.cs
+++ WideWorldImportersBLL/AutoMapper/BusinessLogicProfile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
 using WideWorldImporters.BLL.Models;
+using WideWorldImporters.DAL;
 
 namespace WideWorldImporters.AutoMapper
 {
@@ -10,14 +11,35 @@ namespace WideWorldImporters.AutoMapper
     {
         public BusinessLogicProfile()
         {
-            //CreateMap<Customer, BuyingGroupsBl>();
-            //CreateMap<Store, CitiesBl>();
-            //CreateMap<SalesTerritory, ColorsBl>();
-            //CreateMap<Person, CountriesBl>();
-            //CreateMap<SalesOrderHeader, CustomerCategoriesBl>();
-            //CreateMap<BusinessEntity, CustomersBl>();
-            //CreateMap<Employee, CustomerTransactionsBl>();
-            //CreateMap<Password, PasswordBl>();
+            CreateMap<BuyingGroup, BuyingGroupBl>();
+            CreateMap<City, CityBl>();
+            CreateMap<Color, ColorBl>();
+            CreateMap<Country, CountryBl>();
+            CreateMap<Customer, CustomerBl>();
+            CreateMap<CustomerCategory, CustomerCategoryBl>();
+            CreateMap<CustomerTransaction, CustomerTransactionBl>();
+            CreateMap<DeliveryMethod, DeliveryMethodBl>();
+            CreateMap<Invoice, InvoiceBl>();
+            CreateMap<InvoiceLine, InvoiceLineBl>();
+            CreateMap<Order, OrderBl>();
+            CreateMap<OrderLine, OrderLineBl>();
+            CreateMap<PackageType, PackageTypeBl>();
+            CreateMap<PaymentMethod, PaymentMethodBl>();
+            CreateMap<Person, PersonBl>();
+            CreateMap<PurchaseOrder, PurchaseOrderBl>();
+            CreateMap<PurchaseOrderLine, PurchaseOrderLineBl>();
+            CreateMap<SpecialDeal, SpecialDealBl>();
+            CreateMap<StateProvince, StateProvinceBl>();
+            CreateMap<StockGroup, StockGroupBl>();
+            CreateMap<StockItem, StockItemBl>();
+            CreateMap<StockItemHolding, StockItemHoldingBl>();
+            CreateMap<StockItemStockGroup, StockItemStockGroupBl>();
+            CreateMap<StockItemTransaction, StockItemTransactionBl>();
+            CreateMap<Supplier, SupplierBl>();
+            CreateMap<SupplierCategory, SupplierCategoryBl>();
+            CreateMap<SupplierTransaction, SupplierTransactionBl>();
+            CreateMap<SystemParameter, SystemParameterBl>();
+            CreateMap<TransactionType, TransactionTypeBl>();
         }
     }
 }
